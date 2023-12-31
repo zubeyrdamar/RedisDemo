cbd434038cd2823dfe1caffa09db3e08ecec7404
zubeyr <zubeyrdamar@outlook.com>
2023-12-31 15:14:30 +0300

    In Memory Cache
    * Set
    * Get
    * TryGetValue
    * GetOrCreate
    * Remove

---
+++ RedisApp/RedisApp.Web/Controllers/TimeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace RedisApp.Web.Controllers
+{
+    public class TimeController : Controller
+    {
+        private readonly IMemoryCache memoryCache;
+        public TimeController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public IActionResult Set()
+        {
+            /* 
+            |
+            |   We may want to add or update cache data.
+            |   Before doing that it is a good idea to check if ther is
+            |   any data with the key which we are trying to assign data.
+            |
+            |   In this situation the time is updated if it is not cached
+            |   or the old data is removed from the cache.
+            |
+            |   TryGetValue : tries to find data with the key
+            |                 if data exists then it returns true,
+            |                 if data does not exist then it returns false
+            |
+            */
+
+            if (!memoryCache.TryGetValue("time", out _))
+            {
+                memoryCache.Set<string>("time", DateTime.Now.ToString());
+            }
+
+            return View();
+        }
+
+        public IActionResult Get()
+        {
+            /* 
+            |
+            |   We may try to get data with the given key.
+            |   If there is no data found in the cache with the given
+            |   key, then we should handle the situation.
+            |
+            |   GetOrCreate : if data does not exist with the given key
+            |                 then we assign data to the key.
+            |                 This prevents us from errors.
+            |
+            */
+
+            memoryCache.GetOrCreate<string>("time", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
+            /*
+            |
+            |   Get : gets data with the given key
+            |
+            */
+
+            ViewBag.Time = memoryCache.Get<string>("time");
+
+            /*
+            |
+            |   Remove : removes data with the given key
+            |
+            |   In this example time is removed after caching.
+            |
+            */
+
+            memoryCache.Remove("time");
+
+            return View();
+        }
+    }
+}
--- RedisApp/RedisApp.Web/Program.cs
+++ RedisApp/RedisApp.Web/Program.cs
@@ -3,21 +3,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+/* 
+|-------------------------------------------------------
+| MEMORY CACHE
+|------------------------------------------------------- 
+|
+| To be able to use Redis or any other cache service,
+| we need to add Memory Cache service to our application.
+|
+*/
+
+builder.Services.AddMemoryCache();
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
